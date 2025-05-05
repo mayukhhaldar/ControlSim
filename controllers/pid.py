508ba91686b370efdf66d89d86fede41d09b6ca8
Mayukh Haldar <mhaldar@uwaterloo.ca>
2025-05-04 21:07:41 -0400

    adding some base code

---
+++ controllers/pid.py
@@ -0,0 +1,16 @@
+
+
+
+class PIDController:
+    def __init__(self, p, i, d):
+        self.kp = p
+        self.ki = i
+        self.kd = d
+
+    def computeControl():
+        NotImplemented
+
+    def reset():
+        NotImplemented
+
+    
